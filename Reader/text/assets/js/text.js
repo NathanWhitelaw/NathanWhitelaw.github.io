3fb1bc615c25b0ecd9a8cb68fe8d6078c8fe948c
NathanWhitelaw <Nate@dyn-118-139-61-77.its.monash.edu.au>
2017-05-30 10:28:54 +1000

    reader
    h

---
+++ Reader/text/assets/js/text.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  $('.number').mouseenter(function() {
+    $('.footnote').addClass('showme');
+  });
+  $(document).ready(function() {
+
+  $('.number').mouseleave(function() {
+    $('.footnote').removeClass('showme');
+  });
+
+});
+
+
+
+});
